a85e41c2b877833c2c1e05f28ec9a492199b81bf
zealotzealot <hsh0908y@gmail.com>
2024-10-31 14:02:25 +0900

    Implement serial communication

---
+++ Arduino_Serial/Arduino_Serial.ino
@@ -0,0 +1,50 @@
+#define BUFFER_SIZE 128
+
+#define TRIG_PIN 13
+#define ECHO_PIN 12
+
+#define LED_PIN 8
+
+
+unsigned char packetBuffer[BUFFER_SIZE];
+
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  float sensorValue = getSensorValue();
+
+  int packetSize = Serial.available();
+  if (packetSize) {
+    Serial.readBytes(packetBuffer, min(BUFFER_SIZE, packetSize));
+    bool isMousePressed = packetBuffer[0] == 1;
+    digitalWrite(LED_PIN, isMousePressed ? 1 : 0);
+
+    char message[10];
+    snprintf(message, sizeof(message), "%f\n", sensorValue);
+
+    Serial.write(message, 10);
+  }
+
+  delay(2);
+}
+
+
+float getSensorValue() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  unsigned long duration = pulseIn(ECHO_PIN, HIGH);
+  float distance = (float)duration * 343 / 1000000 / 2;
+
+  return distance;
+}
---
+++ Unity/Assets/Scripts/SerialCommunication.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class SerialCommunication : MonoBehaviour
+{
+    public const int TARGET_FREQUENCY = 10;
+
+
+    public string portName;
+    public int baudRate;
+
+
+    private SerialPort serialPort;
+
+    private float timeFromLastSend;
+
+    private bool isMousePressed;
+    private float sensorValue;
+
+
+    void Start()
+    {
+        if (portName.Length == 0 || baudRate == 0)
+        {
+            Debug.Log("Port Name or Baud Rate is empty.");
+        }
+
+        serialPort = new SerialPort(portName, baudRate);
+        serialPort.DtrEnable = true;
+        serialPort.RtsEnable = true;
+        serialPort.Open();
+    }
+
+
+    void Update()
+    {
+        isMousePressed = Input.GetMouseButton(0);
+
+        GetComponentInChildren<TextMeshPro>().text = string.Format("{0:0.00}", sensorValue);
+
+        int bytesToRead = serialPort.BytesToRead;
+        if (bytesToRead > 0)
+        {
+            byte[] message = new byte[bytesToRead];
+            serialPort.Read(message, 0, bytesToRead);
+
+            DecodeMessageArduinoToUnity(message);
+        }
+
+        timeFromLastSend += Time.deltaTime;
+        if (timeFromLastSend >= 1.0 / TARGET_FREQUENCY)
+        {
+            byte[] message = EncodeMessageUnityToArduino();
+            serialPort.Write(message, 0, message.Length);
+
+            timeFromLastSend = 0;
+        }
+    }
+
+
+    private void OnDestroy()
+    {
+        serialPort.Close();
+    }
+
+
+    private byte[] EncodeMessageUnityToArduino()
+    {
+        byte[] message = new byte[1];
+
+        message[0] = isMousePressed ? (byte) 1 : (byte) 0;
+
+        return message;
+    }
+
+
+    private void DecodeMessageArduinoToUnity(byte[] message)
+    {
+        string decodedString = Encoding.ASCII.GetString(message);
+
+        sensorValue = float.Parse(decodedString);
+    }
+}
